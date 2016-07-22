9d49e9a3a35fa32b84dae33c8ab63f4db93f72e3
Jani Iiskola <jani.iiskola@gmail.com>
2016-07-22 14:07:38 +0300

    problem3 solved

---
+++ problem3/problem3.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/janiiiskola/projectEuler-problems/util"
+)
+
+func main() {
+	defer util.TrackExecutionTime(time.Now())
+
+	n, i, res := 600851475143, 2, 0
+
+	for n > 1 {
+		if n%i == 0 {
+			res = i
+			n = n / i
+		}
+		i++
+	}
+
+	log.Printf("Result: %d", res)
+
+}
